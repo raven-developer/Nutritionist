aff70a5e0dea155cad271f30af96b369616caccf
raven <rjcapiral.designer@gmail.com>
2025-03-30 19:48:16 +0800

    Added up to footer
    Responsive: Pending
    Navigation: Pending
    Other Page: Pending

--- web_assets/js/script.js
+++ web_assets/js/script.js
@@ -8,13 +8,26 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("../web_include/footer.html")
         .then(response => response.text())
         .then(data => document.getElementById("footer").innerHTML = data);
-});
 
-document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll(".w-blog-excerpt").forEach(function (content) {
         let words = content.textContent.trim().split(/\s+/); // Trim and split by spaces
         if (words.length > 20) {
             content.textContent = words.slice(0, 20).join(" ") + "...";
         }
     });
+
+    const toggle_monthly = document.getElementById("toggle-monthly");
+    const toggle_yearly = document.getElementById("toggle-yearly");
+
+    toggle_monthly.addEventListener("click", () => {
+        toggle_monthly.classList.add("enabled");
+        toggle_yearly.classList.remove("enabled");
+    });
+
+    toggle_yearly.addEventListener("click", () => {
+        toggle_yearly.classList.add("enabled");
+        toggle_monthly.classList.remove("enabled");
+    });
+    
+
 });
