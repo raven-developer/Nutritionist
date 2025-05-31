81978d5f4e8b1e56df9bb637270153d0eaa0a705
raven <rjcapiral.designer@gmail.com>
2025-05-31 21:45:48 +0800

    Final Update

--- web_assets/js/script.js
+++ web_assets/js/script.js
@@ -7,17 +7,16 @@ document.addEventListener('DOMContentLoaded', function () {
     .then((response) => response.text())
     .then((data) => (document.getElementById('navbar').innerHTML = data));
 
-  /*// Load Footer
-    fetch("../web_include/footer.html")
-        .then(response => response.text())
-        .then(data => document.getElementById("footer").innerHTML = data);
+  fetch('../web_include/footer.html')
+    .then((response) => response.text())
+    .then((data) => (document.getElementById('footer').innerHTML = data));
 
-    document.querySelectorAll(".w-blog-excerpt").forEach(function (content) {
-        let words = content.textContent.trim().split(/\s+/); // Trim and split by spaces
-        if (words.length > 20) {
-            content.textContent = words.slice(0, 20).join(" ") + "...";
-        }
-    }); */
+  document.querySelectorAll('.w-blog-excerpt').forEach(function (content) {
+    let words = content.textContent.trim().split(/\s+/); // Trim and split by spaces
+    if (words.length > 20) {
+      content.textContent = words.slice(0, 20).join(' ') + '...';
+    }
+  });
 
   const toggle_monthly = document.getElementById('toggle-monthly');
   const toggle_yearly = document.getElementById('toggle-yearly');
